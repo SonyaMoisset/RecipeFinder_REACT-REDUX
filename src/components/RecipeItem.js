13dd372ecfa52a824d8a648c1983be1de29ff5b3
smoisset <smoisset@worldremit.com>
2017-10-26 20:51:40 +0100

    Starring Recipe Items

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -3,16 +3,34 @@ import { connect } from 'react-redux'
 import { favoriteRecipe } from '../actions'
 
 class RecipeItem extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            favorited: false
+        }
+    }
+
+    favoriteRecipe(recipe) {
+        this.props.favoriteRecipe(recipe)
+        this.setState({ favorited: true })
+    }
+
     render() {
         let { recipe } = this.props;
 
         return (
             <div className="recipe-item">
-                <div
-                    onClick={() => this.props.favoriteRecipe(recipe)}
-                    className="star">
-                    &#9734;
-                </div>
+                {
+                    this.state.favorited ?
+                        <div className="star">&#9733;</div>
+                    :
+                        <div
+                            onClick={() => this.favoriteRecipe(recipe)}
+                            className="star">
+                            &#9734;
+                        </div>
+                }
                 <div className="recipe-text">
                     <a href={recipe.href}>
                         <h4>{recipe.title}</h4>
